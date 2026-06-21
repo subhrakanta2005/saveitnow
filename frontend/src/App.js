ff2194b18cb155f75669cb7b2f138fbd27fb4235
subhrakanta2005 <subhrakantbehera8699@gmail.com>
2026-06-21 11:06:08 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import HowItWorks from './pages/HowItWorks';
+import PlatformPage from './pages/PlatformPage';
 import './styles/global.css';
 
 export default function App() {
@@ -14,6 +15,13 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/how-it-works" element={<HowItWorks />} />
+        <Route path="/instagram" element={<PlatformPage platform="instagram" />} />
+        <Route path="/youtube" element={<PlatformPage platform="youtube" />} />
+        <Route path="/tiktok" element={<PlatformPage platform="tiktok" />} />
+        <Route path="/twitter" element={<PlatformPage platform="twitter" />} />
+        <Route path="/facebook" element={<PlatformPage platform="facebook" />} />
+        <Route path="/reddit" element={<PlatformPage platform="reddit" />} />
+        <Route path="/pinterest" element={<PlatformPage platform="pinterest" />} />
       </Routes>
       <Footer />
     </BrowserRouter>
